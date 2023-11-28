f1398a2179ab9e5985376db7690221ef3722b484
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-11-28 18:12:13 +0900

    メニューバー修正

--- src/components/ItemsPage3navbar.js
+++ src/components/ItemsPage3navbar.js
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 const Itemspagenavbar = () => {
 	return (
-		<nav className="flex items-center h-12 pl-3 justify-between bg-[#232e3e]">
-			<div className="max-w-screen flex items-center justify-between mx-auto p-3">
+		<nav className="hidden md:flex h-12 pl-3 bg-[#232e3e]">
+			<div className="max-w-screen flex items-center justify-between mx-auto p-3 overflow-x-scroll">
 				{/* Links for all screen sizes */}
 				<div className="flex items-center space-x-2">
 					<Link href={`/`}>
@@ -53,31 +53,31 @@ const Itemspagenavbar = () => {
 					</Link>
 
 					<Link href={`/category/${`ファッション`}/page/1`} className="">
-						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap ">
 							<span className=" hidden sm:inline pl-2 pr-2">ファッション</span>
 						</div>
 					</Link>
 
 					<Link href={`/category/${`ビューティー`}/page/1`} className="">
-						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap">
 							<span className=" hidden sm:inline pl-2 pr-2">ビューティー</span>
 						</div>
 					</Link>
 
 					<Link href={`/category/${`DIY・工具・ガーデン`}/page/1`} className="">
-						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap ">
 							<span className=" hidden sm:inline pl-2 pr-2">DIY・工具・ガーデン</span>
 						</div>
 					</Link>
 
 					<Link href={`/category/${`おもちゃ`}/page/1`} className="">
-						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap ">
 							<span className=" hidden sm:inline pl-2 pr-2">おもちゃ</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`ペット用品`}/page/1`} className=" overflow-hidden">
-						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
+					<Link href={`/category/${`ペット用品`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap ">
 							<span className=" hidden sm:inline pl-2 pr-2">ペット用品</span>
 						</div>
 					</Link>
