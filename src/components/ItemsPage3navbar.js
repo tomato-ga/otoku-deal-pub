11b93c7393dc39875b8becd0ad0b12a31084500d
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-11-28 18:02:21 +0900

    asinのメニュー3段目調整中

--- src/components/ItemsPage3navbar.js
+++ src/components/ItemsPage3navbar.js
@@ -7,77 +7,77 @@ const Itemspagenavbar = () => {
 				{/* Links for all screen sizes */}
 				<div className="flex items-center space-x-2">
 					<Link href={`/`}>
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap">
 							<span className=" hidden sm:inline pl-2 pr-2 ">ホーム</span>
 						</div>
 					</Link>
 
 					<Link href={`/category/${encodeURIComponent('家電＆カメラ')}/page/1`}>
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap">
 							<span className=" hidden sm:inline pl-2 pr-2">家電＆カメラ</span>
 						</div>
 					</Link>
 					<Link href={`/category/${encodeURIComponent('パソコン・周辺機器')}/page/1`}>
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap">
 							<span className=" hidden sm:inline pl-2 pr-2">パソコン・周辺機器</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`ホーム＆キッチン`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`ホーム＆キッチン`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap">
 							<span className=" hidden sm:inline pl-2 pr-2">ホーム＆キッチン</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`スポーツ＆アウトドア`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`スポーツ＆アウトドア`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap">
 							<span className=" hidden sm:inline pl-2 pr-2">スポーツ＆アウトドア</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`食品・飲料・お酒`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`食品・飲料・お酒`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap">
 							<span className=" hidden sm:inline pl-2 pr-2">食品・飲料・お酒</span>
 						</div>
 					</Link>
-					<Link href={`/category/${`ゲーム`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`ゲーム`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap">
 							<span className=" hidden sm:inline pl-2 pr-2">ゲーム</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`ドラッグストア`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`ドラッグストア`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap">
 							<span className=" hidden sm:inline pl-2 pr-2">ドラッグストア</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`ファッション`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`ファッション`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
 							<span className=" hidden sm:inline pl-2 pr-2">ファッション</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`ビューティー`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`ビューティー`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
 							<span className=" hidden sm:inline pl-2 pr-2">ビューティー</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`DIY・工具・ガーデン`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`DIY・工具・ガーデン`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
 							<span className=" hidden sm:inline pl-2 pr-2">DIY・工具・ガーデン</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`おもちゃ`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`おもちゃ`}/page/1`} className="">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
 							<span className=" hidden sm:inline pl-2 pr-2">おもちゃ</span>
 						</div>
 					</Link>
 
-					<Link href={`/category/${`ペット用品`}/page/1`} className="text-blue-600 hover:text-orange-200">
-						<div className="text-base  md:text-xs lg:text-sm xl:text-lg  text-white hover:text-orange-200 cursor-pointer ">
+					<Link href={`/category/${`ペット用品`}/page/1`} className=" overflow-hidden">
+						<div className="text-base  md:text-base  text-white hover:text-orange-200 cursor-pointer whitespace-nowrap overflow-hidden">
 							<span className=" hidden sm:inline pl-2 pr-2">ペット用品</span>
 						</div>
 					</Link>
