4cf45a1ff2abed7c2227f45887f89c1a97ed1632
tomato-ga <servicedake@gmail.com>
2024-02-23 18:31:47 +0900

    X icon target blank add

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -17,7 +17,7 @@ const TopHeader = (props) => {
 						<span className="text-white px-3 rounded-sm cursor-pointer font-bold">激安特価セール速報</span>
 					</Link>
 					{/* Twitter SVG Icon - Now with white fill */}
-					<Link href="https://twitter.com/otokudeal">
+					<Link href="https://twitter.com/otokudeal" target="_blank">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="ml-2 h-6 w-6">
 							<path d="M8 2H1L9.26086 13.0145L1.44995 21.9999H4.09998L10.4883 14.651L16 22H23L14.3917 10.5223L21.8001 2H19.1501L13.1643 8.88578L8 2ZM17 20L5 4H7L19 20H17Z"></path>
 						</svg>
