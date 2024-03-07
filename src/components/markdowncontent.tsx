aee1df857b267979ecb533fa370cd1942900d9fd
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-03-07 12:05:03 +0900

    マークダウンでAmazonボタン設置完了

--- src/components/markdowncontent.tsx
+++ src/components/markdowncontent.tsx
@@ -1,21 +1,40 @@
+import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
 import remarkGfm from 'remark-gfm'
 
-// propsの型定義を追加
 interface MarkdownContentProps {
 	markdownString: string
 }
 
-// propsオブジェクトを受け取るように修正
 const MarkdownContent: React.FC<MarkdownContentProps> = ({ markdownString }) => {
+	// nodeプロパティを削除して、propsのみを使用
+	const customLink = (props: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
+		// アマゾン.co.jpへのリンクにのみ特別なスタイルを適用し、
+		// すべてのリンクを新しいタブで開くようにする
+		const isAmazonJPLink = props.href?.includes('amazon.co.jp')
+		const className = isAmazonJPLink ? 'markdown-amazon-button' : ''
+
+		return (
+			<a
+				{...props}
+				className={className + (props.className ? ' ' + props.className : '')}
+				target="_blank" // 新しいタブでリンクを開く
+				rel="noopener noreferrer" // セキュリティ対策
+			/>
+		)
+	}
+
 	return (
 		<div className="markdown">
 			<ReactMarkdown
-				children={markdownString} // childrenプロパティにmarkdownStringを渡す
+				children={markdownString}
 				rehypePlugins={[rehypeRaw, rehypeSanitize]}
 				remarkPlugins={[remarkGfm]}
+				components={{
+					a: customLink // リンクにカスタムコンポーネントを適用
+				}}
 			/>
 		</div>
 	)
